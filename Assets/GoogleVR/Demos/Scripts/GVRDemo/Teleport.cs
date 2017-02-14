bdd607f9ea451762c022ffb5ace5b4bcba1cc783
Daniel Schruhl <danielschruhl@gmail.com>
2017-02-14 18:51:32 +0100

    cube is in your face before being transported randomely on click

--- Assets/GoogleVR/Demos/Scripts/GVRDemo/Teleport.cs
+++ Assets/GoogleVR/Demos/Scripts/GVRDemo/Teleport.cs
@@ -1,55 +1,74 @@
-// Copyright 2014 Google Inc. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(Collider))]
-public class Teleport : MonoBehaviour {
-  private Vector3 startingPosition;
+public class Teleport : MonoBehaviour
+{
+    private Vector3 startingPosition;
 
-  public Material inactiveMaterial;
-  public Material gazedAtMaterial;
+    public Material inactiveMaterial;
+    public Material gazedAtMaterial;
 
-  void Start() {
-    startingPosition = transform.localPosition;
-    SetGazedAt(false);
-  }
-
-  void LateUpdate() {
-    GvrViewer.Instance.UpdateState();
-    if (GvrViewer.Instance.BackButtonPressed) {
-      Application.Quit();
+    void Start()
+    {
+        startingPosition = transform.localPosition;
     }
-  }
 
-  public void SetGazedAt(bool gazedAt) {
-    if (inactiveMaterial != null && gazedAtMaterial != null) {
-      GetComponent<Renderer>().material = gazedAt ? gazedAtMaterial : inactiveMaterial;
-      return;
+    void LateUpdate()
+    {
+        GvrViewer.Instance.UpdateState();
+        if (GvrViewer.Instance.BackButtonPressed)
+        {
+            Application.Quit();
+        }
     }
-    GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
-  }
 
-  public void Reset() {
-    transform.localPosition = startingPosition;
-  }
+    public void SetGazedAt(bool gazedAt)
+    {
+        if (inactiveMaterial != null && gazedAtMaterial != null)
+        {
+            GetComponent<Renderer>().material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+            return;
+        }
+        GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+    }
 
-  public void TeleportRandomly() {
-    Vector3 direction = Random.onUnitSphere;
-    direction.y = Mathf.Clamp(direction.y, 0.5f, 1f);
-    float distance = 2 * Random.value + 1.5f;
-    transform.localPosition = direction * distance;
-  }
+    IEnumerator StartTeleport()
+    {
+        // hide
+        Debug.Log("hide");
+        transform.localPosition = new Vector3(30, 30, 30);
+        yield return new WaitForSecondsRealtime(3);
+        Debug.Log("in your face");
+        // show in center camera
+        Vector3 pos = this.CenterScreenPosition();
+        Debug.Log("in your face pos: " + pos.x + ", " + pos.y + ", " + pos.z);
+        transform.localPosition = this.CenterScreenPosition();
+        yield return new WaitForSecondsRealtime(1.5f);
+        Debug.Log("transport");
+        // transport randomely
+        Vector3 direction = Random.onUnitSphere;
+        direction.y = Mathf.Clamp(direction.y, 0.5f, 1f);
+        float distance = 2 * Random.value + 3.5f;
+        Vector3 tempPosition = direction * distance;
+        Debug.Log("Transport from" + transform.localPosition.x + ", " + transform.localPosition.y + ", " + transform.localPosition.z);
+        Debug.Log("Transport to" + tempPosition.x + ", " + tempPosition.y + ", " + tempPosition.z);
+        transform.localPosition = tempPosition;
+    }
+
+    public void Reset()
+    {
+        transform.localPosition = startingPosition;
+    }
+
+    private Vector3 CenterScreenPosition()
+    {
+        //Camera camera = GetComponent<Camera>();
+        return Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 1f));
+    }
+
+    public void TeleportRandomly()
+    {
+        StartCoroutine(StartTeleport());
+    }
 }
