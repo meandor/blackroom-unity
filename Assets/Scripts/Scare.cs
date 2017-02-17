ae69dee59c89305b994862763b2c2a37624ff7d7
Daniel Schruhl <danielschruhl@gmail.com>
2017-02-17 14:00:21 +0100

    add visibility to scares and made the hitbox bigger

--- Assets/Scripts/Scare.cs
+++ Assets/Scripts/Scare.cs
@@ -3,27 +3,25 @@ using System.Collections;
 
 [RequireComponent(typeof(Collider))]
 [RequireComponent(typeof(GvrAudioSource))]
+[RequireComponent(typeof(Renderer))]
 public class Scare : MonoBehaviour
 {
     private Vector3 startingPosition;
     private GvrAudioSource audio;
     private AudioClip jumpScareSound;
-    private AudioClip scare1;
-    private AudioClip scare2;
-    private AudioClip scare3;
+    public AudioClip scareSound;
+    public bool isVisible;
 
     void Awake()
     {
         jumpScareSound = Resources.Load<AudioClip>("Audio/162472__kastenfrosch__scary");
-        scare1 = Resources.Load<AudioClip>("Audio/178072__bsperan__scary-demon-haunting-sound-adam-webb-remix-3");
         audio = GetComponent<GvrAudioSource>();
-        Debug.Log(jumpScareSound);
     }
 
     void Start()
     {
         startingPosition = transform.localPosition;
-        audio.PlayOneShot(scare1, 0.8f);
+        this.ToggleVisibility(isVisible);
     }
 
     void LateUpdate()
@@ -35,10 +33,30 @@ public class Scare : MonoBehaviour
         }
     }
 
-    IEnumerator StartTeleport()
+    public void ScareSequence()
+    {
+        StartCoroutine(StartTeleport());
+    }
+
+    public void Reset()
+    {
+        transform.localPosition = startingPosition;
+    }
+
+    private void ToggleVisibility(bool active)
+    {
+        this.gameObject.SetActive(active);
+        if (active)
+        {
+            audio.PlayOneShot(scareSound, 0.8f);
+        }
+    }
+
+    private IEnumerator StartTeleport()
     {
         // hide
         Debug.Log("hide");
+
         this.Hide();
         yield return new WaitForSecondsRealtime(3);
         Debug.Log("in your face");
@@ -47,52 +65,25 @@ public class Scare : MonoBehaviour
         yield return new WaitForSecondsRealtime(1.5f);
         this.Hide();
         yield return new WaitForSecondsRealtime(4);
-        Debug.Log("transport");
-        // transport randomely
-        this.Reposition();
-        
-    }
-
-    public void Reset()
-    {
-        transform.localPosition = startingPosition;
+        // TODO Show next scare
     }
 
     private Vector3 CenterScreenPosition()
     {
-        //Camera camera = GetComponent<Camera>();
         return Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0, 1f));
-    }
-
-    public void TeleportRandomly()
-    {
-        StartCoroutine(StartTeleport());
     }
 
     private void InYourFace()
     {
         Vector3 pos = this.CenterScreenPosition();
-        audio.Stop();
         Debug.Log("in your face pos: " + pos.x + ", " + pos.y + ", " + pos.z);
         transform.localPosition = this.CenterScreenPosition();
         audio.PlayOneShot(jumpScareSound, 0.4f);
     }
 
-    private void Reposition()
-    {
-        Vector3 direction = Random.onUnitSphere;
-        direction.y = Mathf.Clamp(direction.y, 0.5f, 1f);
-        float distance = 2 * Random.value + 3.5f;
-        Vector3 tempPosition = direction * distance;
-        Debug.Log("Transport from" + transform.localPosition.x + ", " + transform.localPosition.y + ", " + transform.localPosition.z);
-        Debug.Log("Transport to" + tempPosition.x + ", " + tempPosition.y + ", " + tempPosition.z);
-        audio.Stop();
-        transform.localPosition = tempPosition;
-        audio.PlayOneShot(scare1, 0.8f);
-    }
-
     private void Hide()
     {
+        audio.Pause();
         transform.localPosition = new Vector3(30, 30, 30);
     }
 }
