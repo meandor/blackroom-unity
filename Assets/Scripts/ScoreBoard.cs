6fbab301eb5f06cf63880ad8ae641448fcbdec64
Daniel Schruhl <danielschruhl@gmail.com>
2017-02-18 16:16:59 +0100

    add scoreboard

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,28 +1,46 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ScoreBoard : IScare
 {
-    private Vector3 startingPosition;
-    private Quaternion startingRotation;
     public bool isVisible;
+    public float centerXOffset;
+    public float centerYOffset;
+    public float centerZOffset;
+    public float deltaYOffset;
+    public Text scoreTextField;
 
     void Start()
     {
-        startingPosition = transform.localPosition;
-        startingRotation = transform.localRotation;
+        this.scoreTextField.text = "1000";
         this.ToggleVisibility(isVisible);
     }
 
     public override void Reset()
     {
-        transform.localPosition = startingPosition;
-        transform.localRotation = startingRotation;
         this.ToggleVisibility(true);
+        //this.scoreTextField.text = "0";
     }
 
     public override void ToggleVisibility(bool active)
     {
         this.gameObject.SetActive(active);
+        if (active)
+        {
+            this.Reposition();
+        }
+    }
+
+    private Vector3 CenterScreenPosition()
+    {
+        return Camera.main.ViewportToWorldPoint(new Vector3(this.centerXOffset, this.centerYOffset, this.centerZOffset));
+    }
+
+    private void Reposition()
+    {
+        transform.localPosition = this.CenterScreenPosition();
+        float deltaY = Camera.main.transform.localEulerAngles.y + deltaYOffset;
+        transform.localEulerAngles = new Vector3(0.0f, deltaY, 0.0f);
     }
 }
